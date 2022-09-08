94720f9809d2be01dec517d08476b55ef2a718ac
festus flowin <flowinfestus@gmail.com>
2022-09-09 00:58:58 +0300

    UPDATE 20220908 | updated to php7

--- administration/inventory/display_items.php
+++ administration/inventory/display_items.php
@@ -2,7 +2,7 @@
 require_once ('../dBConfig/dBConnect.php');
 $sth = $connect->query("SELECT * FROM tms_items ORDER BY id DESC");
 $data = array();
-while($r = mysql_fetch_assoc($sth)) {
+while($r = mysqli_fetch_assoc($sth)) {
     $data[] = $r;
 }
 print json_encode($data);
